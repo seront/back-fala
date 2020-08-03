fbfb64fa7b3df20fb48c9fdddcd6f5018c600c02
seront <seront.nmmc@gmail.com>
2020-08-02 22:16:27 -0400

    Api respondiendo correctamente

--- back-fala/app.js
+++ back-fala/app.js
@@ -10,14 +10,12 @@ var solicitudRouter = require('./routes/solicitud');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/validaSolicitud', solicitudRouter);
@@ -32,9 +30,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log('ERROR HANDLER',  err.status);
   // render the error page
   res.status(err.status || 500);
+  console.log(err.message);
   res.send('Error ' + err.status);
 });
 
--- back-fala/routes/solicitud.js
+++ back-fala/routes/solicitud.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var validator = require("express-validator");
+var fs = require("fs");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Solicitud valida');
-});
+var checks = [
+  validator.check("rut").not().isEmpty(),
+  validator.check("celular").not().isEmpty(),
+  validator.check("email").not().isEmpty(),
+  validator.check("renta").not().isEmpty(),
+];
+
+var validate = (req, res) => {
+  const errors = validator.validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  fs.writeFile('./output/' + req.body.rut + '.txt', JSON. stringify(req.body), (error) => {
+    res.status(200).json({ status: "ok" });
+  });
+};
+
+router.post('/', checks, validate);
 
 module.exports = router;
