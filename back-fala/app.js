50a45b169f5ab4fdba35f072d0b0bcd74756b71c
seront <seront.nmmc@gmail.com>
2020-08-03 12:31:05 -0400

    instalando cors

--- back-fala/app.js
+++ back-fala/app.js
@@ -1,8 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var solicitudRouter = require('./routes/solicitud');
@@ -16,6 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/validaSolicitud', solicitudRouter);
